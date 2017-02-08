56ea84ec23730df586e7029e1cf45d8bc198c730
Kamil Wróbel <k.wrobel@tvn.pl>
2017-02-08 10:18:09 +0100

    meh

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var mongoose = require('mongoose');
 var UsersRepository = require('./repository/users_repository');
 var User = require('./model/user');
 
-//mongoose.connect('mongodb://janusz:mietek@ds145009.mlab.com:45009/users');
+mongoose.connect('mongodb://janusz:mietek@ds145009.mlab.com:45009/users');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -18,42 +18,42 @@ router.get('/test', function (req, res) {
 });
 
 
-// router.get('/user/:id', function (req, res) {
-//     var id = req.params.id;
-//
-//     UsersRepository.getUser(id, function (err, user) {
-//         if(err){
-//             res.status(err).json();
-//         }else{
-//             res.status(200).json(user);
-//         }
-//     });
-// });
-//
-// router.get('/users', function (req, res) {
-//
-//     UsersRepository.getUsers(function (err, users) {
-//         if(err){
-//             res.status(err).json();
-//         }else{
-//             res.status(200).json(users);
-//         }
-//     });
-// });
-//
-// router.post('/users', function (req, res) {
-//     var user = new User();
-//     user.login = req.body.login;
-//     user.password = req.body.password;
-//
-//     UsersRepository.addUser(user, function (err, user) {
-//         if(err){
-//             res.status(err).json();
-//         }else{
-//             res.status(200).json(user);
-//         }
-//     });
-// });
+router.get('/user/:id', function (req, res) {
+    var id = req.params.id;
+
+    UsersRepository.getUser(id, function (err, user) {
+        if(err){
+            res.status(err).json();
+        }else{
+            res.status(200).json(user);
+        }
+    });
+});
+
+router.get('/users', function (req, res) {
+
+    UsersRepository.getUsers(function (err, users) {
+        if(err){
+            res.status(err).json();
+        }else{
+            res.status(200).json(users);
+        }
+    });
+});
+
+router.post('/users', function (req, res) {
+    var user = new User();
+    user.login = req.body.login;
+    user.password = req.body.password;
+
+    UsersRepository.addUser(user, function (err, user) {
+        if(err){
+            res.status(err).json();
+        }else{
+            res.status(200).json(user);
+        }
+    });
+});
 
 app.use('/api', router);
 
